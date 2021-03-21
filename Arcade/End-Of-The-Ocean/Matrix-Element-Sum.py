e75062bfff95ca92c619f502d8dcaaf03882370a
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-03-21 01:29:00 +0100

    Delete Arcade/End-Of-The-Ocean directory

--- Arcade/End-Of-The-Ocean/Matrix-Element-Sum.py
+++
@@ -1,9 +0,0 @@
-def matrixElementsSum(matrix):
-    r = 0
-    for i in range(1, len(matrix)):
-        for k in range(len(matrix[i-1])):
-            if matrix[i-1][k] == 0:
-                matrix[i][k] = 0
-    for i in matrix:
-        r += sum(i)
-    return r 
