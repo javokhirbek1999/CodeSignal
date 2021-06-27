813d7b62dff4affc2d445ecaf8fa1c9b8dbdd916
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-06-27 20:57:45 +0500

    Create Knapsack-Light.py

---
+++ Arcade/Intro/Dark-Wilderness/Knapsack-Light.py
@@ -0,0 +1,22 @@
+def knapsackLight(value1, weight1, value2, weight2, maxW):
+    if min(weight1, weight2) > maxW:
+        return 0
+    if weight1+weight2 > maxW:
+        if value1 > value2 and weight1 <= maxW:
+            return value1
+        elif value1 > value2 and weight1 > maxW:
+            if weight2 <= maxW:
+                return value2
+            else:
+                return 0
+        elif value2 > value1 and weight2 <= maxW:
+            return value2
+        elif value2 > value1 and weight2 > maxW:
+            if weight1 <= maxW:
+                return value1
+            else:
+                return 0
+        else:
+            return value2
+    else:
+        return value1+value2
